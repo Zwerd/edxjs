729f9872441c1b19446cff8fb5f9123719f535ee
guy.zwerdling <guy.zwerdling@gmail.com>
2018-08-02 20:46:10 +0300

    index3

--- index2.js
+++ index2.js
@@ -1,49 +1,17 @@
 var express = require('express');
-
 var app = express();
 
-
-
-
-
 app.use('/', (req, res) => {
-
-  
-
 	var name = req.query.name;
-
-
-
 	res.status(200).type('html');
-
-
-
 	if (name) {
-
-	    res.write('Hi, ' + name + ", it's nice to see you.");
-
+    res.write('Hi, ' + name + ", it's nice to see you.");
 	}
-
 	else {
-
-	    res.write('Welcome, guest!');
-
+    res.write('Welcome, guest!');
 	}
-
-	
-
 	res.end();
-
-	
-
     });
-
-
-
-
-
 app.listen(3000, () => {
-
 	console.log('Listening on port 3000');
-
 });

3aff9f9b287e3bc70852fbe9c26df7b773efa2f9
GuyZwer <guy.zwerdling@gmail.com>
2018-08-27 07:32:53 +0300

    update for 3 of that course

---
+++ MongoDB/3/Book.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+// where does the db name come from? I just made this up
+mongoose.connect('mongodb://localhost:27017/myBook');
+
+var Schema = mongoose.Schema;
+
+var authorSchema = new Schema({
+	name: String,
+	affiliation: String
+    });
+
+var bookSchema = new Schema({
+	title: {type: String, required: true, unique: true},
+	year: Number,
+	authors: [authorSchema]
+    });
+
+
+module.exports = mongoose.model('Book', bookSchema);

99d86b012d0cdd028db644ff6b0d534f14a99942
GuyZwer <guy.zwerdling@gmail.com>
2018-09-13 20:35:27 +0300

    update

--- HarvardX-CS50M/JavaScriptES6/0-closureBug.js
+++ HarvardX-CS50M/JavaScriptES6/0-closureBug.js
@@ -2,7 +2,9 @@ function makeArr(){
       const arr = []
             for (var i=0; i< 5 ; i++){
               console.log(i)
+              
               arr.push(function(){console.log(i)})
+              console.log(arr,arr[i]())
             }
       console.log(arr)    
       return arr

881387de9909b7322dc1e4627b09c9d81ae73294
GuyZwer <guy.zwerdling@gmail.com>
2018-09-13 20:23:46 +0300

    update

--- HarvardX-CS50M/overviewJavascript/0-closureBug2.js
+++ HarvardX-CS50M/overviewJavascript/0-closureBug2.js
@@ -1,8 +1,10 @@
 function makeArr(){
       const arr = []
-            for (let i=0; i< 5 ; i++){
-                      arr.push(function(){console.log(i)})
-            }
+            
+        for (let i=0; i< 5 ; i++){
+          console.log(i)
+          arr.push(function(){console.log(i)})
+        }
           
       return arr
 }
@@ -10,4 +12,7 @@ function makeArr(){
 const arr = makeArr()
 
 arr[0]()
-
+arr[1]()
+arr[2]()
+arr[3]()
+arr[4]()

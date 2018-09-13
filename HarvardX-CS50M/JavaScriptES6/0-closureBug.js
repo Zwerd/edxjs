ee9471dfa517a60c750b4740ebfb9ded85dd0717
GuyZwer <guy.zwerdling@gmail.com>
2018-09-13 20:28:42 +0300

    update

---
+++ HarvardX-CS50M/JavaScriptES6/0-closureBug.js
@@ -0,0 +1,17 @@
+function makeArr(){
+      const arr = []
+            for (var i=0; i< 5 ; i++){
+              console.log(i)
+              arr.push(function(){console.log(i)})
+            }
+      console.log(arr)    
+      return arr
+}
+
+const arr = makeArr()
+
+arr[0]()
+arr[1]()
+arr[2]()
+arr[3]()
+arr[4]()

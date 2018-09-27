6fdaca700de6cebde9a7aa00f0f35aa89ee7c4d4
GuyZwer <guy.zwerdling@gmail.com>
2018-09-27 22:26:00 +0300

    update the project1 script with green and red line on the li element and DELETE button - dosnt work

--- HarvardX-CS50M/Lecture1/project0/script.js
+++ HarvardX-CS50M/Lecture1/project0/script.js
@@ -11,37 +11,45 @@ const uncheckedCountSpan = document.getElementById('unchecked-count')
 const itemList = []
 let checkedNumber = parseInt(0)
 let item,itemsNumber,element;
-
+let todonumber = 0
 
 function newTodo() {
+  todonumber += 1
+  console.log(todonumber)
   item = prompt("Enter TODO for your list: ")
   itemList.push(item)
   itemsNumber = parseInt(itemList.length)
-  element = '<li type="checkbox" id="'+item
-            +'"name="TODO" value="'+item
+  element = '<li type="checkbox" id="'+item + todonumber
+            +'"name="TODO" value="'+item + todonumber
             +'"onClick="changeColor(this)">'+item
-            +'</li>'
+            +'<button id="deletebutton" onclick="removeItem('+item + todonumber+')">Delete</button></li><br>'
   list.innerHTML += element
   itemCountSpan.innerHTML = itemsNumber
   uncheckedCountSpan.innerHTML = itemsNumber - checkedNumber
-  document.getElementById(item).style.backgroundColor = 'red'
+  document.getElementById(item+todonumber).style.backgroundColor = 'red'
 }
 
 
-function changeColor(element) {
-  console.log(element.style.backgroundColor)
-  if(element.style.backgroundColor == 'red'){
-    element.style.backgroundColor = 'green'
+function changeColor(item) {
+  console.log(item.style.backgroundColor)
+  if(item.style.backgroundColor == 'red'){
+    item.style.backgroundColor = 'green'
     checkedNumber += parseInt(1)
     console.log('check')
   }else{
-    element.style.backgroundColor = 'red'
+    item.style.backgroundColor = 'red'
     checkedNumber -= parseInt(1)
   }
   uncheckedCountSpan.innerHTML = itemsNumber - checkedNumber
 }
 function removeItem(item){
+  console.log("delete was clicked",item)
   var ul = document.getElementById("todo-list");
-  var element = document.getElementById(item);
-  ul.removeChild(element);
+  ul.removeChild(item);
+  itemList.splice(itemList.indexOf(item))
+  console.log(itemList.indexOf(item))
+  itemsNumber = parseInt(itemList.length)
+  uncheckedCountSpan.innerHTML = itemsNumber
+  console.log(itemsNumber,uncheckedCountSpan.innerHTML)
+
 }

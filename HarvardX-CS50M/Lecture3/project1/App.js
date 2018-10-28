618695106378c6aea8716924fa1321ed4aacf458
zwerd <guy.zwerdling@gmail.com>
2018-10-28 20:25:40 +0200

    update

--- HarvardX-CS50M/Lecture3/project1/App.js
+++ HarvardX-CS50M/Lecture3/project1/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import {vibrate} from './utils'
 
 let seconds = ''
 
@@ -7,10 +8,13 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      minutes:25,
-      seconds:59,
-      startTimer:true
-
+      minutes:'00',
+      seconds:'10',
+      startTimer:true,
+      button:'Stop',
+      status:'TIME TO WORK',
+      work:{mins:'25',secs:'00',},
+      break:{mins:'05',secs:'00'}
     };
   }
 
@@ -31,6 +35,17 @@ timer = () => {
   if(minutes.length==1){
     minutes = '0'+minutes
   }
+  if(minutes == '00' && seconds =='00'){
+    vibrate()
+    clearInterval(this.startInterval)
+    this.setState({startTimer:'restart',button:'Start Again'})
+  }
+  if(this.state.minutes == this.state.work.mins && this.state.seconds == this.state.work.secs){
+    this.setState({status:'TIME TO WORK',})
+  }
+  if(this.state.minutes == this.state.break.mins && this.state.seconds == this.state.break.secs){
+    this.setState({status:'BREAKING TIME',})
+  }
   this.setState({
     seconds: seconds,
     minutes: minutes
@@ -40,23 +55,46 @@ timer = () => {
 stopTimer = () => {
   if(this.state.startTimer == true){
     clearInterval(this.startInterval)
-    this.setState({startTimer:false})
-  }else{
+    this.setState({startTimer:false,button:'Start'})
+  }else if(this.state.startTimer == false){
     this.startInterval = setInterval(this.timer,1000)
-    this.setState({startTimer:true})
+    this.setState({startTimer:true,button:'Stop'})
+  }else{
+    this.setState({
+      seconds: '00',
+      minutes: '25'
+    })
+    this.startInterval = setInterval(this.timer,1000)
+    this.setState({startTimer:true,button:'Stop'})
+  }
 }
+
+resetTimer = () => {
+  this.setState({
+    seconds:'00',
+    minutes:'25',
+    startTimer:true
+  })
+  clearInterval(this.startInterval)
+  this.startInterval = setInterval(this.timer,1000)
 }
 
     render() {
+      console.log(this.state.work.mins)
     return (
-      <View style={styles.container}>
-          <Text style={styles.title}>WORK TIMER</Text>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.title}>{this.state.status}</Text>
           <Text style={styles.timer}>
               {this.state.minutes}:{this.state.seconds}
           </Text>
           <View style={styles.row}>
-            <Button title="Pause" onPress={this.stopTimer}/>
-            <Button title="Reset"/>
+            <TouchableOpacity style={styles.button} onPress={this.stopTimer}>
+              <Text style={styles.text}>{this.state.button}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={this.resetTimer}>
+              <Text style={styles.text}>Reset</Text>
+            </TouchableOpacity>
           </View>
           <View style={styles.row}>
             <Text style={styles.text}>Work Time: </Text>
@@ -65,14 +103,14 @@ stopTimer = () => {
               style={styles.textInput}
               underlineColorAndroid='transparent'
               placeholder='25'
-              onChangeText={(text)=>this.setState({timer:{minutes:props.text}})}
+              onChangeText={(text)=>this.setState({work:{mins:text}})}
              />
             <Text>Secs:</Text>
             <TextInput
               style={styles.textInput}
               underlineColorAndroid='transparent'
-              placeholder='0'
-              onChangeText={(text)=>this.setState({timer:{minutes:props.text}})}
+              placeholder='00'
+              onChangeText={(text)=>this.setState({work:{sec:text}})}
              />
           </View>
           <View style={styles.row}>
@@ -81,18 +119,19 @@ stopTimer = () => {
             <TextInput
               style={styles.textInput}
               underlineColorAndroid='transparent'
-              placeholder='5'
-              onChangeText={(text)=>this.setState({timer:{minutes:props.text}})}
+              placeholder='05'
+              onChangeText={(text)=>this.setState({break:{mins:text}})}
              />
             <Text>Secs:</Text>
             <TextInput
               style={styles.textInput}
               underlineColorAndroid='transparent'
-              placeholder='0'
-              onChangeText={(text)=>this.setState({timer:{minutes:props.text}})}
+              placeholder='00'
+              onChangeText={(text)=>this.setState({break:{secs:text}})}
              />
           </View>
-      </View>
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -109,7 +148,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   timer:{
-    fontSize: 70,
+    fontSize: 100,
   },
   row:{
     flexDirection:'row',
@@ -121,8 +160,8 @@ const styles = StyleSheet.create({
     fontSize:20,
   },
   textInput:{
-    height:20,
-    width:30,
+    padding:5,
+    fontSize:20,
     borderColor:'black',
     borderWidth:1,
     textAlign:'center'
@@ -130,5 +169,12 @@ const styles = StyleSheet.create({
   flex:{
     flex:1,
     flexDirection:'row'
+  },
+  button:{
+    margin:3,
+    padding:20,
+    backgroundColor: 'lightblue',
+    borderWidth:2,
+    borderRadius:10,
   }
 });

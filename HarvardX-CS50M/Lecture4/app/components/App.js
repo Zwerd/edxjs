ea4c678ccdb8a68eabefc6a602c7153d57ed6ab6
zwerd <guy.zwerdling@gmail.com>
2018-11-06 15:52:27 +0200

    there is some issue with the SORT button - the function SORT doesnt run by onPress

--- HarvardX-CS50M/Lecture4/app/components/App.js
+++ HarvardX-CS50M/Lecture4/app/components/App.js
@@ -1,31 +1,60 @@
 import React from 'react';
-import { Button, ScrollView, StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Button, TouchableOpacity, FlatList, ScrollView, StyleSheet, Text, View, StatusBar } from 'react-native';
 
-
-import contacts from './contacts'
+import contacts, {compareNames} from './contacts'
+import Row from './Row'
 
 export default class App extends React.Component {
   state = {
     showContacts: false,
+    contacts: contacts,
   }
 
   toggleContacts = () => {
     this.setState(prevState => ({showContacts: !prevState.showContacts}))
   }
 
+  sort = () => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts].sort(compareNames)
+    }))
+  }
+
+  renderItem = (obj) => <Row {...(obj.item)}/>
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Button title="toggle contacts" onPress={this.toggleContacts} />
-      </View>
+      return(
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={this.toggleContacts}>
+           <Text style={styles.text}>toggle contacts</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={this.sort}>
+           <Text style={styles.text}>Sort</Text>
+          </TouchableOpacity>
+          {this.state.showContacts && (
+            <FlatList
+                renderItem={this.renderItem}
+                data={this.state.contacts}
+                keyExtractor={(item) => item.toString()}
+            />
+          )}
+        </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: StatusBar.currentHeight,
   },
+  button:{
+    alignItems: 'center',
+    padding: 1
+  },
+  text:{
+    fontSize: 20,
+    color: 'deepskyblue'
+  }
 });
